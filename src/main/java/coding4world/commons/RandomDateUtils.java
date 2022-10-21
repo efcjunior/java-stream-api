5743a182b7267620e375a1c2509dfdd165d3b8c8
Everson Cunha <everson.c.junior@gmail.com>
2022-10-21 08:38:48 -0300

    Adds getDateUntilNow in RandomDateUtils class and unit test for it.

--- src/main/java/coding4world/commons/RandomDateUtils.java
+++ src/main/java/coding4world/commons/RandomDateUtils.java
@@ -1,16 +1,22 @@
 package coding4world.commons;
 
-import java.time.Instant;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomDateUtils {
 
-    public static void main(String[] args) {
-        System.out.println(Instant.ofEpochSecond(date().getEpochSecond()));
+    public static LocalDate between(LocalDate startInclusive, LocalDate endExclusive) {
+        long startEpochDay = startInclusive.toEpochDay();
+        long endEpochDay = endExclusive.toEpochDay();
+        long randomDay = ThreadLocalRandom.current().nextLong(startEpochDay, endEpochDay);
+
+        return LocalDate.ofEpochDay(randomDay);
     }
 
-    public static Instant date() {
-        return Instant.MIN;
+    public static LocalDate getDateUntilNow() {
+        LocalDate start = LocalDate.MIN;
+        LocalDate end = LocalDate.now();
+        return between(start, end);
     }
 }
--- src/test/java/coding4world/commons/RandomDateUtilsTest.java
+++ src/test/java/coding4world/commons/RandomDateUtilsTest.java
@@ -2,7 +2,14 @@ package coding4world.commons;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RandomDateUtilsTest {
 
-
+   @Test
+    public void getDateUntilNow(){
+        assertThat(RandomDateUtils.getDateUntilNow()).isBetween(LocalDate.MIN, LocalDate.now());
+    }
 }
